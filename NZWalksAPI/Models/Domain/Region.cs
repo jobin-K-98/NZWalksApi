f2045f08b63f9a5d34435599146c1d7e0db14a51
Jobin K Jose <jobinkjose1998@gmail.com>
2024-12-30 07:02:39 -0500

    Initial Commit

---
+++ NZWalksAPI/Models/Domain/Region.cs
@@ -0,0 +1,10 @@
+﻿namespace NZWalksAPI.Models.Domain
+{
+    public class Region
+    {
+        public Guid id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
